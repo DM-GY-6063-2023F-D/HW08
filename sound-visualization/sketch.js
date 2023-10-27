530c302a60f5abd935620ee1861274ed70dfa9ee
thiagohersan <thiago.hersan+github@gmail.com>
2023-10-26 23:12:02 -0400

    factors out all scaling factors into sliders

--- sound-visualization/sketch.js
+++ sound-visualization/sketch.js
@@ -2,9 +2,12 @@ let STEP_SIZE = 8;
 
 let song;
 let samples;
+let sampleAvgRaw;
 
 let pos = [];
 
+let stepSlider;
+let avgSlider;
 let radiusSlider;
 
 function preload() {
@@ -22,30 +25,47 @@ function setup() {
   textFont("sans-serif");
   textSize(14);
 
+  stepSlider = createSlider(0.5, 3, 1, 0.1);
+  stepSlider.position(width - 200, 40);
+
+  avgSlider = createSlider(0, 50, 2, 0.2);
+  avgSlider.position(width - 200, 90);
+
   radiusSlider = createSlider(1, 8, 8, 0.1);
-  radiusSlider.position(width - 200, 50);
+  radiusSlider.position(width - 200, 140);
 
   samples = song.getPeaks();
-  let avgS = samples.reduce(sum, 0) / samples.length / 1.2;
+  sampleAvgRaw = samples.reduce(sum, 0) / samples.length;
 
   let p = { x: 0, y: 0 };
   let moveLength = floor(samples.length / 2);
 
   for (let i = 0; i < moveLength; i++) {
-    p.x += samples[i] - avgS;
-    p.y += samples[moveLength + i] - avgS;
+    p.x += samples[i] - sampleAvgRaw;
+    p.y += samples[moveLength + i] - sampleAvgRaw;
     pos.push({ x: p.x, y: p.y });
   }
 }
 
-function scaledPolar(p0, p1, radius) {
+function normalize(p) {
   return {
-    x0: radius * p0.x * cos(radians(2 * STEP_SIZE * p0.y)),
-    y0: radius * p0.x * sin(radians(2 * STEP_SIZE * p0.y)),
-    x1: radius * p1.x * cos(radians(2 * STEP_SIZE * p1.y)),
-    y1: radius * p1.x * sin(radians(2 * STEP_SIZE * p1.y)),
+    x: (p.x - sampleAvg) * stepSize,
+    y: (p.y - sampleAvg) * stepSize,
   };
 }
+
+function scaledPolar(p0, p1) {
+  return {
+    x0: radius * p0.x * cos(radians(p0.y)),
+    y0: radius * p0.x * sin(radians(p0.y)),
+    x1: radius * p1.x * cos(radians(p1.y)),
+    y1: radius * p1.x * sin(radians(p1.y)),
+  };
+}
+
+let stepSize;
+let sampleAvg;
+let radius;
 
 function draw() {
   background(0);
@@ -53,20 +73,27 @@ function draw() {
   fill(255);
 
   let mi = frameCount % pos.length;
-  let radius = (2 * STEP_SIZE) / (9 - radiusSlider.value());
+
+  stepSize = stepSlider.value() * STEP_SIZE;
+  sampleAvg = avgSlider.value() * sampleAvgRaw;
+  radius = 1 / (9 - radiusSlider.value());
+
+  let normPos = pos.map(normalize);
 
   push();
   translate(width / 2, height / 2);
 
   for (let i = 1; i < mi; i++) {
-    let p0 = pos[i];
-    let p1 = pos[i - 1];
+    let p0 = normPos[i];
+    let p1 = normPos[i - 1];
 
-    let polar = scaledPolar(p0, p1, radius);
+    let polar = scaledPolar(p0, p1);
     line(polar.x0, polar.y0, polar.x1, polar.y1);
   }
   pop();
 
   noStroke();
-  text("polar radius", width - 200, 30);
+  text("step scale", width - 200, 40);
+  text("average scale", width - 200, 90);
+  text("polar radius", width - 200, 140);
 }
