de87067660988c8084a4e30976bd9d1555eddde6
thiagohersan <thiago.hersan+github@gmail.com>
2023-10-26 22:41:24 -0400

    remove cartesian line from sound viz

--- sound-visualization/sketch.js
+++ sound-visualization/sketch.js
@@ -28,50 +28,45 @@ function setup() {
   samples = song.getPeaks();
   let avgS = samples.reduce(sum, 0) / samples.length / 1.2;
 
-  let p = { x: width / 2, y: height / 2 };
-  pos.push({ x: p.x, y: p.y });
-
+  let p = { x: 0, y: 0 };
   let moveLength = floor(samples.length / 2);
 
   for (let i = 0; i < moveLength; i++) {
-    p.x = (p.x + STEP_SIZE * (samples[i] - avgS)) % width;
-    p.y = (p.y + STEP_SIZE * (samples[moveLength + i] - avgS)) % height;
+    p.x += samples[i] - avgS;
+    p.y += samples[moveLength + i] - avgS;
     pos.push({ x: p.x, y: p.y });
   }
 }
 
+function scaledPolar(p0, p1, radius) {
+  return {
+    x0: radius * p0.x * cos(radians(2 * STEP_SIZE * p0.y)),
+    y0: radius * p0.x * sin(radians(2 * STEP_SIZE * p0.y)),
+    x1: radius * p1.x * cos(radians(2 * STEP_SIZE * p1.y)),
+    y1: radius * p1.x * sin(radians(2 * STEP_SIZE * p1.y)),
+  };
+}
+
 function draw() {
   background(0);
+  stroke(255);
+  fill(255);
 
   let mi = frameCount % pos.length;
-  let rScale = 9 - radiusSlider.value();
+  let radius = (2 * STEP_SIZE) / (9 - radiusSlider.value());
+
+  push();
+  translate(width / 2, height / 2);
 
   for (let i = 1; i < mi; i++) {
     let p0 = pos[i];
     let p1 = pos[i - 1];
-    let a = map(mi - i, 0, pos.length / 8, 255, 0);
-    a = 255;
 
-    if (abs(p0.x - p1.x) <= STEP_SIZE && abs(p0.y - p1.y) <= STEP_SIZE) {
-      stroke(255, a);
-      fill(255, a);
-      line(p0.x, p0.y, p1.x, p1.y);
-
-      let x0 = (p0.x / rScale) * cos(radians(p0.y));
-      let y0 = (p0.x / rScale) * sin(radians(p0.y));
-      let x1 = (p1.x / rScale) * cos(radians(p1.y));
-      let y1 = (p1.x / rScale) * sin(radians(p1.y));
-
-      push();
-      translate(width / 2, height / 2);
-      stroke(255);
-      fill(255);
-      line(x0, y0, x1, y1);
-      pop();
-    }
+    let polar = scaledPolar(p0, p1, radius);
+    line(polar.x0, polar.y0, polar.x1, polar.y1);
   }
-  ellipse(pos[mi].x, pos[mi].y, 8, 8);
+  pop();
 
   noStroke();
-  text("radius scale", width - 200, 30);
+  text("polar radius", width - 200, 30);
 }
